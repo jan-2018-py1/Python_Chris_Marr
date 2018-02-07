df64f1975a7e4d23b4c5aa2b43c5f1af58273a63
cmarr31 <chris@sitefuelusa.com>
2018-02-07 13:26:38 -0600

    OOP notes.

---
+++ week_5/OOP/users.py
@@ -0,0 +1,15 @@
+class User(object):
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.logged = False
+    
+    def login(self):
+        self.logged = True
+        print self.name + " is logged in."
+        return self
+
+new_user = User("Anna", "anna800@gmail.com")
+print new_user.name
+print new_user.email
+
