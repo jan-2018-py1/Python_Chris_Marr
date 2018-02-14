d61ee2388d763094bf2ceee7c7dfa2298c387aa0
cmarr31 <chris@sitefuelusa.com>
2018-02-13 20:53:17 -0600

    .

---
+++ week_5/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/cmarr31/python_round_2/Python_Chris_Marr/week_5/venv/bin/python2.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
