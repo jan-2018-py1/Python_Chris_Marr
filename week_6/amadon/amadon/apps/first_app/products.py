e1dba4299ae6d375e5d96dc34569861371079229
cmarr31 <chris@sitefuelusa.com>
2018-03-19 23:14:19 -0500

    Amadon assignment.

---
+++ week_6/amadon/amadon/apps/first_app/products.py
@@ -0,0 +1,22 @@
+items = [
+    {
+        "id": 1,
+        "name": "Raspberry Pi",
+        "price": 35,
+    },
+    {
+        "id": 2,
+        "name": "Nintendo Switch",
+        "price": 350,
+    },
+    {
+        "id": 3,
+        "name": "Legend of Zelda",
+        "price": 60,
+    },
+    {
+        "id": 4,
+        "name": "Screen and button PCB set",
+        "price": 55,
+    },
+]
